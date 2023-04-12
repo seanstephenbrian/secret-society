df8493d81535cedf4ab279bcd85ce2d602978fa1
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-04-12 10:22:41 -0400

    add bcryptjs password hashing

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -1,23 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 router.get('/', (req, res) => res.render('sign-up'));
 
 router.post('/', async (req, res, next) => {
-    try {
-        const user = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password
-        });
-        const result = await user.save();
-        res.redirect('./');
-    } catch(err) {
-        return next(err);
-    };
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        if (err) {
+            return next(err);
+        } else {
+            try {
+                const user = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    password: hashedPassword
+                });
+                const result = await user.save();
+                res.redirect('./');
+            } catch(err) {
+                return next(err);
+            };
+        }
+    });
 });
 
 module.exports = router;
