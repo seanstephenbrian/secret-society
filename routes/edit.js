829514caef17664fe141f9edcbb0ea85b7ef2076
sean stephen brian <seanstephenbrian@gmail.com>
2023-04-13 15:46:21 -0400

    add validation / error msgs to edit message form

--- routes/edit.js
+++ routes/edit.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 const Message = require('../models/message');
 
@@ -14,24 +15,59 @@ router.get('/:id', function(req, res, next) {
         });
 });
 
-router.post('/', async function(req, res, next) {
-    try {
-        const message = await Message.findByIdAndUpdate(
-            req.body.id, 
-            { 
-                title: req.body.title,
-                body: req.body.body
+router.post(
+    '/', 
+    [
+        body('title')
+            .trim()
+            .isString()
+            .isLength( { min: 1 } )
+            .withMessage('Message title cannot be empty.')
+            .isLength( { max: 100 })
+            .withMessage('Message title cannot be longer than 100 characters'),
+        body('body')
+            .trim()
+            .isString()
+            .isLength( { min: 1 } )
+            .withMessage('Message body cannot be empty.')
+            .isLength( { max: 1000 } )
+            .withMessage('Message body cannot be longer than 1000 characters')
+    ],
+    async function(req, res, next) {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.render(
+                'edit-message', 
+                { 
+                    errors: errors.array(), 
+                    message: { 
+                        id: req.body.id,
+                        title: req.body.title,
+                        body: req.body.body
+                    } 
+                }
+            );
+        }
+
+        try {
+            const message = await Message.findByIdAndUpdate(
+                req.body.id, 
+                { 
+                    title: req.body.title,
+                    body: req.body.body
+                }
+            );
+            if (!message) {
+                const err = new Error('Unable to update message.');
+                return next(err);
             }
-        );
-        if (!message) {
-            const err = new Error('Unable to update message.');
+            res.redirect('/');
+        } catch(err) {
             return next(err);
         }
-        res.redirect('/');
-    } catch(err) {
-        return next(err);
     }
-    
-});
+);
 
 module.exports = router;
